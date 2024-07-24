c610b7d93796953d2dc166788bb31b0dce32448d
Michael Johnson <michael@icefoganalytics.com>
2024-07-24 11:24:28 -0700

    Auth in production

--- src/web/src/plugins/auth.ts
+++ src/web/src/plugins/auth.ts
@@ -1,5 +1,5 @@
 import { createAuth0 } from "@auth0/auth0-vue";
-import { domain, client_id, audience } from "../../auth-config.json";
+import { production, development } from "../../auth-config.json";
 
 // The "@auth0/auth0-vue" library composables to load and manage
 // authentication information.  In components this information is available
@@ -9,11 +9,15 @@ import { domain, client_id, audience } from "../../auth-config.json";
 // of the way that composables work.  So, the authStore loads in @/App.vue to
 // work around this issue.
 
+let config = production;
+
+if (window.location.host == "localhost:8080") config = development;
+
 export const AuthHelper = createAuth0({
-  domain,
-  clientId: client_id,
+  domain: config.domain,
+  clientId: config.client_id,
   authorizationParams: {
-    audience,
+    audience: config.audience,
     redirect_uri: window.location.origin,
   },
   // Uncomment the following line to use the config from .env file
