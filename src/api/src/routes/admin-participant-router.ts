da9a2888b9f8617c142147577d98163ccf75e08b
Michael Johnson <michael@icefoganalytics.com>
2026-02-09 17:03:56 -0700

    Update admin-participant-router.ts
    Reformat and fix

--- src/api/src/routes/admin-participant-router.ts
+++ src/api/src/routes/admin-participant-router.ts
@@ -5,33 +5,40 @@ import { isInteger, isNil } from "lodash";
 
 export const adminParticipantRouter = express.Router();
 
-adminParticipantRouter.get("/:SID/:DEMOGRAPHIC_GROUP?", async (req: Request, res: Response) => {
-  let { SID, DEMOGRAPHIC_GROUP } = req.params;
-  let GROUP = DEMOGRAPHIC_GROUP == "null" ? null : DEMOGRAPHIC_GROUP;
+adminParticipantRouter.get(
+  "/:SID/:DEMOGRAPHIC_GROUP?",
+  async (req: Request, res: Response) => {
+    let { SID, DEMOGRAPHIC_GROUP } = req.params;
+    let GROUP = DEMOGRAPHIC_GROUP == "null" ? null : DEMOGRAPHIC_GROUP;
 
-  if (!isNil(GROUP) && isInteger(parseInt(GROUP))) {
-    let list = await db("PARTICIPANT_DATA")
-      .withSchema(DB_SCHEMA)
-      .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
-      .where("PARTICIPANT.SID", SID)
-      .where("PARTICIPANT.DEMOGRAPHIC_GROUP_ID", GROUP);
+    if (!isNil(GROUP) && isInteger(parseInt(GROUP))) {
+      let list = await db("PARTICIPANT_DATA")
+        .withSchema(DB_SCHEMA)
+        .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
+        .where("PARTICIPANT.SID", SID)
+        .where("PARTICIPANT.DEMOGRAPHIC_GROUP_ID", GROUP);
 
-    res.json({ data: list });
-  } else {
-    let list = await db("PARTICIPANT_DATA")
-      .withSchema(DB_SCHEMA)
-      .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
-      .where("PARTICIPANT.SID", SID);
+      res.json({ data: list });
+    } else {
+      let list = await db("PARTICIPANT_DATA")
+        .withSchema(DB_SCHEMA)
+        .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
+        .where("PARTICIPANT.SID", SID);
 
-    res.json({ data: list });
-  }
-});
+      res.json({ data: list });
+    }
+  },
+);
 
 adminParticipantRouter.post("/", async (req: Request, res: Response) => {
   let { addresses, survey, fileData, prefix, demographicGroup } = req.body;
 
-  let demographicInsert: { TOKEN: string | null; DEMOGRAPHIC: string; NVALUE: number | null; TVALUE: string | null }[] =
-    [];
+  let demographicInsert: {
+    TOKEN: string | null;
+    DEMOGRAPHIC: string;
+    NVALUE: number | null;
+    TVALUE: string | null;
+  }[] = [];
   const demographicGroupData: {
     TOKEN: string | null;
     DEMOGRAPHIC: string;
@@ -54,6 +61,20 @@ adminParticipantRouter.post("/", async (req: Request, res: Response) => {
     }
   }
 
+  const existAddressQuery = db("PARTICIPANT_DATA")
+    .withSchema(DB_SCHEMA)
+    .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
+    .where("PARTICIPANT.SID", survey)
+    .whereNotNull("EMAIL")
+    .select("EMAIL");
+
+  if (isNil(demographicGroup))
+    existAddressQuery.whereNull("DEMOGRAPHIC_GROUP_ID");
+  else existAddressQuery.where("DEMOGRAPHIC_GROUP_ID", demographicGroup);
+
+  let existingAddresses = await existAddressQuery;
+  let existingList = existingAddresses.map((e) => e.EMAIL);
+
   if (fileData && fileData.headers && fileData.headers.length > 0) {
     let demographicList = await db("DEMOGRAPHIC_TYPE").withSchema(DB_SCHEMA);
     let demographics = demographicList.map((d) => d.DEMOGRAPHIC);
@@ -62,16 +83,6 @@ adminParticipantRouter.post("/", async (req: Request, res: Response) => {
     fileData.headers.shift();
 
     for (let records of fileData.records) {
-      let existingAddresses = await db("PARTICIPANT_DATA")
-        .withSchema(DB_SCHEMA)
-        .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
-        .where("PARTICIPANT.SID", survey)
-        .where("DEMOGRAPHIC_GROUP_ID", demographicGroup)
-        .whereNotNull("EMAIL")
-        .select("EMAIL");
-
-      let existingList = existingAddresses.map((e) => e.EMAIL);
-
       let email = records.shift();
       if (existingList.includes(email)) continue;
 
@@ -86,31 +97,35 @@ adminParticipantRouter.post("/", async (req: Request, res: Response) => {
         let value = records[i];
 
         let demo = header;
-        if (!demographics.includes(demo)) return res.status(500).send(`Invalid demographic: ${demo}`);
+        if (!demographics.includes(demo))
+          return res.status(500).send(`Invalid demographic: ${demo}`);
 
         let nval = isNumeric(value) ? Number(value) : null;
         let tval = isNumeric(value) ? null : value;
-        demographicInsert.push({ TOKEN: token, DEMOGRAPHIC: demo, NVALUE: nval, TVALUE: tval });
+        demographicInsert.push({
+          TOKEN: token,
+          DEMOGRAPHIC: demo,
+          NVALUE: nval,
+          TVALUE: tval,
+        });
       }
 
-      await db("PARTICIPANT")
+      await db("PARTICIPANT").withSchema(DB_SCHEMA).insert({
+        TOKEN: token,
+        SID: survey,
+        CREATE_DATE: new Date(),
+        DEMOGRAPHIC_GROUP_ID: demographicGroup,
+      });
+      await db("PARTICIPANT_DATA")
         .withSchema(DB_SCHEMA)
-        .insert({ TOKEN: token, SID: survey, CREATE_DATE: new Date(), DEMOGRAPHIC_GROUP_ID: demographicGroup });
-      await db("PARTICIPANT_DATA").withSchema(DB_SCHEMA).insert({ TOKEN: token, EMAIL: email });
+        .insert({ TOKEN: token, EMAIL: email });
       if (demographicInsert.length > 0)
-        await db("PARTICIPANT_DEMOGRAPHIC").withSchema(DB_SCHEMA).insert(demographicInsert);
+        await db("PARTICIPANT_DEMOGRAPHIC")
+          .withSchema(DB_SCHEMA)
+          .insert(demographicInsert);
     }
   } else {
     for (let address of addresses) {
-      let existingAddresses = await db("PARTICIPANT_DATA")
-        .withSchema(DB_SCHEMA)
-        .innerJoin("PARTICIPANT", "PARTICIPANT_DATA.TOKEN", "PARTICIPANT.TOKEN")
-        .where("PARTICIPANT.SID", survey)
-        .where("DEMOGRAPHIC_GROUP_ID", demographicGroup)
-        .whereNotNull("EMAIL")
-        .select("EMAIL");
-
-      let existingList = existingAddresses.map((e) => e.EMAIL);
       if (existingList.includes(address)) continue;
 
       let token = makeToken(prefix);
@@ -119,48 +134,78 @@ adminParticipantRouter.post("/", async (req: Request, res: Response) => {
         value.TOKEN = token;
       }
 
-      await db("PARTICIPANT")
+      await db("PARTICIPANT").withSchema(DB_SCHEMA).insert({
+        TOKEN: token,
+        SID: survey,
+        CREATE_DATE: new Date(),
+        DEMOGRAPHIC_GROUP_ID: demographicGroup,
+      });
+      await db("PARTICIPANT_DATA")
         .withSchema(DB_SCHEMA)
-        .insert({ TOKEN: token, SID: survey, CREATE_DATE: new Date(), DEMOGRAPHIC_GROUP_ID: demographicGroup });
-      await db("PARTICIPANT_DATA").withSchema(DB_SCHEMA).insert({ TOKEN: token, EMAIL: address });
+        .insert({ TOKEN: token, EMAIL: address });
 
       if (demographicInsert.length > 0)
-        await db("PARTICIPANT_DEMOGRAPHIC").withSchema(DB_SCHEMA).insert(demographicInsert);
+        await db("PARTICIPANT_DEMOGRAPHIC")
+          .withSchema(DB_SCHEMA)
+          .insert(demographicInsert);
     }
   }
 
   res.json({ data: {} });
 });
 
-adminParticipantRouter.delete("/:TOKEN", async (req: Request, res: Response) => {
-  let { TOKEN } = req.params;
+adminParticipantRouter.delete(
+  "/:TOKEN",
+  async (req: Request, res: Response) => {
+    let { TOKEN } = req.params;
 
-  await db("PARTICIPANT_DEMOGRAPHIC").withSchema(DB_SCHEMA).where({ TOKEN }).delete();
-  await db("PARTICIPANT_DATA").withSchema(DB_SCHEMA).where({ TOKEN }).delete();
-  await db("PARTICIPANT").withSchema(DB_SCHEMA).where({ TOKEN }).delete();
+    await db("PARTICIPANT_DEMOGRAPHIC")
+      .withSchema(DB_SCHEMA)
+      .where({ TOKEN })
+      .delete();
+    await db("PARTICIPANT_DATA")
+      .withSchema(DB_SCHEMA)
+      .where({ TOKEN })
+      .delete();
+    await db("PARTICIPANT").withSchema(DB_SCHEMA).where({ TOKEN }).delete();
 
-  res.json({ data: {} });
-});
+    res.json({ data: {} });
+  },
+);
 
-adminParticipantRouter.delete("/:SID/stale", async (req: Request, res: Response) => {
-  let { SID } = req.params;
+adminParticipantRouter.delete(
+  "/:SID/stale",
+  async (req: Request, res: Response) => {
+    let { SID } = req.params;
 
-  let staleOnes = await await db("PARTICIPANT")
-    .withSchema(DB_SCHEMA)
-    .innerJoin("PARTICIPANT_DATA", "PARTICIPANT.TOKEN", "PARTICIPANT_DATA.TOKEN")
-    .whereNull("RESPONSE_DATE")
-    .where({ SID });
+    let staleOnes = await await db("PARTICIPANT")
+      .withSchema(DB_SCHEMA)
+      .innerJoin(
+        "PARTICIPANT_DATA",
+        "PARTICIPANT.TOKEN",
+        "PARTICIPANT_DATA.TOKEN",
+      )
+      .whereNull("RESPONSE_DATE")
+      .where({ SID });
 
-  for (const stale of staleOnes) {
-    await db("PARTICIPANT_DATA").withSchema(DB_SCHEMA).where({ TOKEN: stale.TOKEN }).update({ EMAIL: null });
-  }
+    for (const stale of staleOnes) {
+      await db("PARTICIPANT_DATA")
+        .withSchema(DB_SCHEMA)
+        .where({ TOKEN: stale.TOKEN })
+        .update({ EMAIL: null });
+    }
 
-  res.json({ data: {} });
-});
+    res.json({ data: {} });
+  },
+);
 
 export function makeToken(prefix: string) {
-  const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
-  const randomArray = Array.from({ length: 64 }, (v, k) => chars[Math.floor(Math.random() * chars.length)]);
+  const chars =
+    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const randomArray = Array.from(
+    { length: 64 },
+    (v, k) => chars[Math.floor(Math.random() * chars.length)],
+  );
 
   let randomString = `${prefix}_${randomArray.join("")}`;
   randomString = randomString.replace(/^_/, "");
